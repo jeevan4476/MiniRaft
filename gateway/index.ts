3f7d34dfd8a08f9d95d957a496f5e7369fe67bfb
jeevan4476 <jeevanr2431@gmail.com>
2026-03-21 13:27:39 +0530

    feat: init ws

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,8 +1,23 @@
 import { Hono } from 'hono'
+import { upgradeWebSocket, websocket } from 'hono/bun'
 import pino from 'pino'
 
 const app = new Hono()
 const log = pino({ transport: { target: 'pino-pretty' } })
+
+app.get('/ws', upgradeWebSocket((_) => {
+  return {
+    onOpen(data, ws) {
+      log.info("client connected");
+    },
+    onMessage(data, ws) {
+      log.info(`message received: ${data.data}`);
+    },
+    onClose(_, ws) {
+      log.info("client disconnected");
+    }
+  }
+}))
 
 app.get('/status', (c) => {
   log.info('gateway healthcheck')
@@ -15,4 +30,5 @@ log.info(`Gateway starting on port ${port}`)
 export default {
   port,
   fetch: app.fetch,
+  websocket
 }
